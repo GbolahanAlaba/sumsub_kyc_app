6dcba35db55a946c8d8edb6340f9e1fffa79b087
gbolahan <thegbolahanalaba@gmail.com>
2024-10-03 19:27:16 +0100

    Added new url

--- App_Sumsub/urls.py
+++ App_Sumsub/urls.py
@@ -8,7 +8,8 @@ from . views import *
 urlpatterns = [
    path('create-applicant/', SumsubViewSet.as_view({'post': 'create_applicant'}), name='applicant-creation'),
    path('add-id-document/<str:pk>/', SumsubViewSet.as_view({'post': 'add_document'}), name='document-add'),
-   path('applicant-status/<str:pk>/', SumsubViewSet.as_view({'get': 'get_applicant_status'}), name='applicant-creation'),
+   path('get-verification-status/<str:pk>/', SumsubViewSet.as_view({'get': 'get_applicant_verification_status'}), name='verification-status'),
+   path('get-saved-status/<str:pk>/', SumsubViewSet.as_view({'get': 'get_saved_verification_data'}), name='saved-verification'),
 ]
 
 # urlpatterns = [
