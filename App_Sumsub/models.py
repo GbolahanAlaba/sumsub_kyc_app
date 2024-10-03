860a57b8468731ed50142a7641c3e0a828148101
gbolahan <thegbolahanalaba@gmail.com>
2024-10-03 18:27:09 +0100

    Model creation and migrations applied

--- App_Sumsub/models.py
+++ App_Sumsub/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class VerificationStatus(models.Model):
+    applicant_id = models.CharField(max_length=255, unique=True)
+    country = models.CharField(max_length=255)
+    id_doc_type = models.CharField(max_length=255)
+    image_ids = models.JSONField()  # Store image IDs as a list
+    image_review_results = models.JSONField(null=True, blank=True)  # Store review results as JSON
+    forbidden = models.BooleanField(default=False)
+    partial_completion = models.BooleanField(null=True, blank=True)
+    step_statuses = models.JSONField(null=True, blank=True)  # Store step statuses as JSON
+    image_statuses = models.JSONField(null=True, blank=True)  # Store image statuses as JSON
+    selfie = models.JSONField(null=True, blank=True)  # Store selfie status, if applicable
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.applicant_id
