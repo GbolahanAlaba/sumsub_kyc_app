41eca83dfb9c3a4bfaf4b30248c436b33923416c
gbolahan <thegbolahanalaba@gmail.com>
2024-10-03 19:25:24 +0100

    Verification status saved data added

--- App_Sumsub/serializers.py
+++ App_Sumsub/serializers.py
@@ -3,3 +3,10 @@ from . models import *
 from rest_framework.response import Response
 from rest_framework import serializers, validators
 
+
+
+class VerificationSerializer(serializers.Serializer):
+
+    class Meta:
+        model = VerificationStatus
+        fields = '__all__'
